6b50bc6c7fec4383cf1c4875a74dbd253a4cb2ed
宮島大知 <miyajima.shrine@gmail.com>
2019-05-12 22:08:22 +0900

    intervalを追加

--- check/check.go
+++ check/check.go
@@ -2,6 +2,7 @@ package check
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Candy_Wheel_Alert/notify"
 	"github.com/Candy_Wheel_Alert/robot"
@@ -18,10 +19,14 @@ func NewCheck(notifyer notify.Notifyer) *Check {
 }
 
 func (c *Check) Checktran(richLists []robot.RichList) {
-	fmt.Println("--------------------------------------")
-	for _, richList := range richLists {
-		fmt.Println("Address:", richList.Address)
-		fmt.Println(richList.Wallet)
-	}
-	fmt.Println("--------------------------------------")
+	ticker := time.NewTicker(3 * time.Second)
+	for {
+        select {
+		case <-ticker.C:
+			for _, richList := range richLists {
+				fmt.Println("Address:", richList.Address)
+				fmt.Println(richList.Wallet)
+			}
+        }
+    }
 }
